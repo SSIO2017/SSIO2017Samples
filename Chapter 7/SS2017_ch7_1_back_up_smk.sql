33feb4e0e60e49ed350a14d6fcd3628133405fa4
Randolph West <rabryst@users.noreply.github.com>
2018-01-30 14:52:56 -0700

    Chapter 7 (Securing the server and its data)
    Initial file to create folder

---
+++ Chapter 7/SS2017_ch7_1_back_up_smk.sql
@@ -0,0 +1,5 @@
+-- Backing up the SMK to the local hard drive
+
+BACKUP SERVICE MASTER KEY TO FILE = 'c:\SecureLocation\service_master_key'   
+   ENCRYPTION BY PASSWORD = '<UseAReallyStrongPassword>';  
+GO
